762ed0f77ac197a5dabe1c85f47383d0feac8622
Nebiy Negash <neba048@gmail.com>
2023-01-01 22:20:44 -0600

    updated main.js added instead of index.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,30 +1,83 @@
-// * Update the timer
-
+// * Update the timer 01
 const timer = {
         pomodoro: 25,
         shortBreak: 5,
         longBreak: 15,
-        longBreakIntervals: 4,
+        longBreakInterval: 4,
+        sessions: 0,
 };
 
 let interval;
 
+const mainButton = document.getElementById('js-btn');
+mainButton.addEventListener('click', () => {
+        const { action } = mainButton.dataset;
+        if (action === 'start') {
+                startTimer();
+        } else {
+                stopTimer();
+        }
+});
+
 const modeButtons = document.querySelector('#js-mode-buttons');
 modeButtons.addEventListener('click', handleModel);
+
+function getRemainingTime(endTime) {
+        const currentTime = Date.parse(new Date());
+        const difference = endTime - currentTime;
+
+        const total = Number.parseInt(difference / 1000, 10);
+        const minutes = Number.parseInt((total / 60) % 60, 10);
+        const seconds = Number.parseInt(total % 60, 10);
+
+        return {
+                total,
+                minutes,
+                seconds,
+        };
+}
 
 function startTimer() {
         let { total } = timer.remainingTime;
         const endTime = Date.parse(new Date()) + total * 1000;
 
+        // eslint-disable-next-line no-plusplus
+        if (timer.mode === 'pomodoro') timer.sessions++;
+
+        mainButton.dataset.action = 'stop';
+        mainButton.textContent = 'stop';
+        mainButton.classList.add('active');
+
         interval = setInterval(() => {
                 timer.remainingTime = getRemainingTime(endTime);
                 updateClock();
-
                 total = timer.remainingTime.total;
                 if (total <= 0) {
                         clearInterval(interval);
+
+                        switch (timer.mode) {
+                                case 'pomodoro':
+                                        if (timer.sessions % timer.longBreakInterval === 0) {
+                                                switchMode('longBreak');
+                                        } else {
+                                                switchMode('shortBreak');
+                                        }
+                                        break;
+                                default:
+                                        switchMode('pomodoro');
+                        }
+
+                        startTimer();
                 }
         }, 1000);
+}
+
+function stopTimer() {
+        clearInterval(interval);
+
+        mainButton.dataset.action = 'start';
+        mainButton.textContent = 'start';
+        mainButton.classList.remove('active');
 }
 
 function updateClock() {
@@ -36,25 +89,46 @@ function updateClock() {
         const sec = document.getElementById('js-seconds');
         min.textContent = minutes;
         sec.textContent = seconds;
+
+        const text = timer.mode === 'pomodoro' ? 'Get back to work' : 'Take a break';
+        document.title = `${minutes}:${seconds} - ${text}`;
+
+        const progress = document.getElementById('js-progress');
+        progress.value = timer[timer.mode] * 60 - timer.remainingTime.total;
 }
 
+// ? The switchMode function adds two new properties to the timer object at the top.
+
 function switchMode(mode) {
+        // ? mode property set to the current mode from the three mode buttons(pomodoro, shortBreak,longBreak)
         timer.mode = mode;
+
+        // ? remainingTime property set on the timer object, this is an object with three properties of its own.
         timer.remainingTime = {
                 total: timer[mode] * 60,
                 minutes: timer[mode],
                 seconds: 0,
         };
 
+        // ! need a fix on this.
         document.querySelectorAll('button[data-mode]').forEach((e) => e.classList.remove('active'));
         document.querySelector(`[data-mode=${mode}]`).classList.add('active');
-        // document.body.style.backgroundColor = `var(--${mode})`;
+        document.getElementById('js-progress').setAttribute('max', timer.remainingTime.total);
         updateClock();
 }
+
+// ? event delegation to detect a click on any of the mode buttons.
 
 function handleModel(event) {
         const { mode } = event.target.dataset;
 
+        // ? switchMode function is applied here with a value of the data-mode attribute as its only argument.
+
         if (!mode) return;
         switchMode(mode);
+        stopTimer();
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+        switchMode('pomodoro');
+});
